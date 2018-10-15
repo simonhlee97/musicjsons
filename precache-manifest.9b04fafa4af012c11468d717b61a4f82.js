0c3ba39595ca7e104be20e7164be47e174c56c3f
Simon Lee <simonhlee97@hotmail.com>
2018-10-15 12:03:58 -0400

    Updates

---
+++ precache-manifest.9b04fafa4af012c11468d717b61a4f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8bf49aeff5102afccd5",
+    "url": "/musicjsons/static/js/runtime~main.b8bf49ae.js"
+  },
+  {
+    "revision": "cbfe2ddfa6ae868ef00c",
+    "url": "/musicjsons/static/js/main.cbfe2ddf.chunk.js"
+  },
+  {
+    "revision": "8e0146420e353464afda",
+    "url": "/musicjsons/static/js/1.8e014642.chunk.js"
+  },
+  {
+    "revision": "cbfe2ddfa6ae868ef00c",
+    "url": "/musicjsons/static/css/main.714d1858.chunk.css"
+  },
+  {
+    "revision": "3182b0ee4d8b64faa238eee931559418",
+    "url": "/musicjsons/index.html"
+  }
+];
